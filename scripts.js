597d2c8454719830ade47529c5006e73fdbc6317
T-Khoury <120677926+T-Khoury@users.noreply.github.com>
2023-02-08 17:28:53 -0800

    Add clear and cleareverything functionality

--- scripts.js
+++ scripts.js
@@ -33,9 +33,60 @@ function operate(operator, a, b) {
 console.log(operate(subtract, 6, 3));
 
 
+
+
+function getNumber(button) {
+    (calculator.display == 0) 
+    ? calculator.display = parseInt(button.id) 
+    : calculator.display += button.id;
+
+}
+
+function refreshDisplay() {
+    displayWindow.textContent = calculator.display;
+}
+
+function displayNumber() {
+    getNumber(this);
+    refreshDisplay();
+}
+
+function clear() {
+    calculator.display = calculator.display.slice(0,-1);
+    refreshDisplay();
+}
+
+function clearEverything() {
+    calculator.display = 0;
+    refreshDisplay();
+}
+
+const calculator = {
+    display: 0,
+};
+
+const numButtons = document.querySelectorAll('.number');
+numButtons.forEach((button) => {
+    button.addEventListener('click', displayNumber);
+})
+
+const displayWindow = document.querySelector('.window');
+
+const backSpace = document.querySelector('#clear');
+backSpace.addEventListener('click', clear);
+
+const resetDisplay = document.querySelector('#cleareverything');
+resetDisplay.addEventListener('click', clearEverything)
+
+
+
+
+
+
 /* A "result" object will be created with:
     number 1:
     number 2:
+    operator:
 
     input steps
 
@@ -43,16 +94,20 @@ console.log(operate(subtract, 6, 3));
 
     (if more number button presses here append number(s) to end of result.number1)
 
-    operator button press will save number from display variable to result.number1 
+    operator button press will save number from display variable to result.number1 and operator function to result.operator
 
-    2nd number button press will reassign display variable
+    2nd number button press will reassign display variable to that number
 
     (if more number button presses here append number(s) to end of display variable
 
 
     -- if there is a decimal ' . ' in one of the numbers append 1st instance of a decimal and then do no nothing when button is pressed if there is already a decimal in the number
 
+    then:
 
+    pressing equals will save display variable to #2, call result.operator(number1, number2), sand save the result to display and then to number 1 and clear number 2
+
+    pressing another operator before first one has been called will 
 
     if there is a number 1 and an operator and a 2nd operator button or = is pushed, store display variable as result.number2 and call operator(result.number1, result.number2) and store result as display variable, then store display variable as result.number1, store the next operator button
     
@@ -61,3 +116,4 @@ console.log(operate(subtract, 6, 3));
 
     add display div and add current number 
 
+    */
